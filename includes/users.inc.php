635fec0335d253a3310a9ea0c99ada80dcabdf29
BravoClassic <gerald.akorli@gmail.com>
2021-04-23 21:25:03 +0530

    removed MYSQLI_USE_RESULT from mysqli_query

--- includes/users.inc.php
+++ includes/users.inc.php
@@ -2,7 +2,7 @@
 session_start();
 include_once 'db.inc.php';
 $sender_id =$_SESSION['unique_id'];
-$sql_query = mysqli_query($conn, "SELECT * FROM khaters WHERE NOT unique_id={$sender_id} ORDER BY fname",MYSQLI_USE_RESULT);
+$sql_query = mysqli_query($conn, "SELECT * FROM khaters WHERE NOT unique_id={$sender_id} ORDER BY fname");
 // $result = mysqli_store_result($sql_query);
 $output;
 if (mysqli_num_rows($sql_query) == 1) {
