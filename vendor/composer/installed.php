61a4ace397f2cf4f4cfea38736b06d8cd69f9dcf
BravoClassic <gerald.akorli@gmail.com>
2021-04-23 21:13:39 +0530

    specified the php version in composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '61bf92df8098edd1492a10d0a0d8062e6c3350f5',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '61bf92df8098edd1492a10d0a0d8062e6c3350f5',
+    ),
+  ),
+);
